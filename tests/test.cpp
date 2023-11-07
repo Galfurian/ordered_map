8a520162623b1e2bd03f9398c42a6e380064e4a8
Enrico Fraccaroli (Galfurian) <enry.frak@gmail.com>
2023-11-07 10:20:27 -0500

    Update tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -18,16 +18,18 @@ inline int get_choice(char *argument)
 inline bool check(const Table &table, const std::string &key, int value)
 {
     Table::const_iterator it = table.find(key);
-    if (it == table.end())
+    if (it == table.end()) {
         return false;
+    }
     return it->second == value;
 }
 
 inline bool check(const Table &table, std::size_t position, int value)
 {
     Table::const_iterator it = table.at(position);
-    if (it == table.end())
+    if (it == table.end()) {
         return false;
+    }
     return it->second == value;
 }
 
@@ -45,9 +47,19 @@ int run_test_0()
     table.set("b", 2);
     table.set("c", 3);
     // Run the checks.
-    return (table.size() == 3) &&
-           check(table, "a", 1) && check(table, "b", 2) && check(table, "c", 3) &&
-           check(table, 0, 1) && check(table, 1, 2) && check(table, 2, 3);
+    if (table.size() != 3) {
+        std::cerr << "Table size is wrong.\n";
+        return 1;
+    }
+    if (!check(table, "a", 1) || !check(table, "b", 2) || !check(table, "c", 3)) {
+        std::cerr << "The key->value association is wrong.\n";
+        return 1;
+    }
+    if (!check(table, 0, 1) || !check(table, 1, 2) || !check(table, 2, 3)) {
+        std::cerr << "The position->value association is wrong (after sort).\n";
+        return 1;
+    }
+    return 0;
 }
 
 int run_test_1()
@@ -61,9 +73,19 @@ int run_test_1()
     // Change a value.
     table.set("c", 5);
     // Run the checks.
-    return (table.size() == 3) &&
-           check(table, "a", 1) && check(table, "b", 2) && check(table, "c", 5) &&
-           check(table, 0, 1) && check(table, 1, 2) && check(table, 2, 5);
+    if (table.size() != 3) {
+        std::cerr << "Table size is wrong.\n";
+        return 1;
+    }
+    if (!check(table, "a", 1) || !check(table, "b", 2) || !check(table, "c", 5)) {
+        std::cerr << "The key->value association is wrong.\n";
+        return 1;
+    }
+    if (!check(table, 0, 1) || !check(table, 1, 2) || !check(table, 2, 5)) {
+        std::cerr << "The position->value association is wrong (after sort).\n";
+        return 1;
+    }
+    return 0;
 }
 
 int run_test_2()
@@ -77,9 +99,19 @@ int run_test_2()
     // Remove a key.
     table.erase("c");
     // Run the checks.
-    return (table.size() == 2) &&
-           check(table, "a", 1) && check(table, "b", 2) && !check(table, "c", 5) &&
-           check(table, 0, 1) && check(table, 1, 2) && !check(table, 2, 5);
+    if (table.size() != 2) {
+        std::cerr << "Table size is wrong.\n";
+        return 1;
+    }
+    if (!check(table, "a", 1) || !check(table, "b", 2) || check(table, "c", 3)) {
+        std::cerr << "The key->value association is wrong.\n";
+        return 1;
+    }
+    if (!check(table, 0, 1) || !check(table, 1, 2) || check(table, 2, 3)) {
+        std::cerr << "The position->value association is wrong.\n";
+        return 1;
+    }
+    return 0;
 }
 
 int run_test_3()
@@ -94,9 +126,19 @@ int run_test_3()
     table.erase("c");
     table.set("d", 7);
     // Run the checks.
-    return (table.size() == 3) &&
-           check(table, "a", 1) && check(table, "b", 2) && !check(table, "c", 5) && check(table, "d", 7) &&
-           check(table, 0, 1) && check(table, 1, 2) && !check(table, 2, 5) && check(table, 2, 7);
+    if (table.size() != 3) {
+        std::cerr << "Table size is wrong.\n";
+        return 1;
+    }
+    if (!check(table, "a", 1) || !check(table, "b", 2) || check(table, "c", 3) || !check(table, "d", 7)) {
+        std::cerr << "The key->value association is wrong.\n";
+        return 1;
+    }
+    if (!check(table, 0, 1) || !check(table, 1, 2) || check(table, 2, 3) || !check(table, 2, 7)) {
+        std::cerr << "The position->value association is wrong.\n";
+        return 1;
+    }
+    return 0;
 }
 
 int run_test_4()
@@ -108,22 +150,32 @@ int run_test_4()
     table.set("b", 2);
     table.set("a", 3);
     // Check the number of elements.
-    if (table.size() != 3)
+    if (table.size() != 3) {
+        std::cerr << "Table size is wrong.\n";
         return 1;
+    }
     // Run the basic checks.
-    if (check(table, "a", 3) && check(table, "b", 2) && check(table, "c", 1))
+    if (!check(table, "a", 3) || !check(table, "b", 2) || !check(table, "c", 1)) {
+        std::cerr << "The key->value association is wrong.\n";
         return 1;
+    }
     // Run the index-based test.
-    if (check(table, 0, 1) && check(table, 1, 2) && check(table, 2, 3))
+    if (!check(table, 0, 1) || !check(table, 1, 2) || !check(table, 2, 3)) {
+        std::cerr << "The position->value association is wrong.\n";
         return 1;
+    }
     // Sort the table.
     table.sort(compare);
     // Run the basic checks again, the key-value association should stay the same.
-    if (check(table, "a", 3) && check(table, "b", 2) && check(table, "c", 1))
+    if (!check(table, "a", 3) || !check(table, "b", 2) || !check(table, "c", 1)) {
+        std::cerr << "The key->value association is wrong (after sort).\n";
         return 1;
+    }
     // Run again the index-based test, which should find the values swapped.
-    if (check(table, 0, 3) && check(table, 1, 2) && check(table, 2, 1))
+    if (!check(table, 0, 3) || !check(table, 1, 2) || !check(table, 2, 1)) {
+        std::cerr << "The position->value association is wrong (after sort).\n";
         return 1;
+    }
     return 0;
 }
 
@@ -131,16 +183,21 @@ int main(int argc, char *argv[])
 {
     if (argc == 2) {
         int choice = get_choice(argv[1]);
-        if (choice == 0)
-            return !run_test_0();
-        if (choice == 1)
-            return !run_test_1();
-        if (choice == 2)
-            return !run_test_2();
-        if (choice == 3)
-            return !run_test_3();
-        if (choice == 4)
-            return !run_test_4();
+        if (choice == 0) {
+            return run_test_0();
+        }
+        if (choice == 1) {
+            return run_test_1();
+        }
+        if (choice == 2) {
+            return run_test_2();
+        }
+        if (choice == 3) {
+            return run_test_3();
+        }
+        if (choice == 4) {
+            return run_test_4();
+        }
     }
     return 1;
 }
